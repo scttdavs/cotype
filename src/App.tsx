b703fc4c438789e94867407ed227eb2d52171796
Scott Davis <sc.ttdav.s@gmail.com>
2020-04-06 14:21:57 -0400

    add peerjs to connect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Peer from "peerjs";
+
 import './App.css';
 
 import { UnControlled as CodeMirror} from "react-codemirror2";
@@ -7,6 +9,18 @@ import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/material.css';
 
 function App() {
+  useEffect(() => {
+    const peer = new Peer(undefined, {
+      config: {
+        // this is default, or run your own
+        iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
+      }
+    });
+    peer.on('open', (id) => {
+      console.log("ID", id);
+    });
+  }, []);
+
   return (
     <CodeMirror
       value='<h1>I ♥ react-codemirror2</h1>'
