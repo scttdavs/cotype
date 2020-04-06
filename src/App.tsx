53a23ee563c3b8c5536efbdfd2115479dab22112
Scott Davis <sc.ttdav.s@gmail.com>
2020-04-06 18:36:57 -0400

    start connecting poc

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,39 @@ import 'codemirror/theme/material.css';
 
 function App() {
   useEffect(() => {
+    // connect as a peer, no matter what
     const peer = new Peer(undefined, {
-      config: {
-        // this is default, or run your own
-        iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
-      }
+      // key: 'lwjd5qra8257b9',
+      host: "/",
+      port: 9000,
+      path: '/signal',
+      debug: 2
     });
     peer.on('open', (id) => {
       console.log("ID", id);
+
+      // if joining a new doc, connect to that peer
+      const peerId = window.location.pathname.slice(1);
+      console.log("Peer id", peerId);
+      if (peerId !== "") {
+        const conn = peer.connect(peerId, { reliable: true });
+        console.log(conn);
+        conn.on('open', () => {
+          console.log("Open to ", peerId);
+          conn.send("HI!");
+        });
+
+        conn.on('data', function(data) {
+          console.log('Received', data);
+        });
+      }
+    });
+
+    peer.on('connection', (conn) => {
+      console.log("Someone is connecting", conn.metadata);
+      conn.on('data', (data) => {
+        console.log('Received', data);
+      });
     });
   }, []);
 
