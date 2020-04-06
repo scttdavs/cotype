3029f6e6a9a6f819c6c52e340b49370fec193a2a
Scott Davis <sc.ttdav.s@gmail.com>
2020-04-06 12:49:08 -0400

    add codemirror

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { UnControlled as CodeMirror} from "react-codemirror2";
+
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CodeMirror
+      value='<h1>I ♥ react-codemirror2</h1>'
+      options={{
+        mode: 'xml',
+        theme: 'material',
+        lineNumbers: true
+      }}
+      onChange={(editor, data, value) => {
+      }}
+    />
   );
 }
 
